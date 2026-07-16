6c3f53bb7e77575bc5048a27b3cd2730ffe63e6e
MiguelDev-GH <miguelchagasmaciel@gmail.com>
2026-07-16 20:01:40 -0300

    chore: configura deploy e corrige roteamento 404 para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/website/', // Importante para o GitHub Pages (nome do repositório)
 })
